d931f33f78c7dc22165c50ee944c973299e58c0e
Manish Hengaju <manish.hju@gmail.com>
2026-05-17 17:37:45 +0545

    fix spinner folder structure

--- frontend/chatapp/src/routes/ProtectedRoute.jsx
+++ frontend/chatapp/src/routes/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import Spinner from "./components/Spinner";
+import Spinner from "../components/Spinner";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
--- frontend/chatapp/src/routes/PublicRoute.jsx
+++ frontend/chatapp/src/routes/PublicRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import Spinner from "./components/Spinner";
+import Spinner from "../components/Spinner";
 
 const PublicRoute = ({ children }) => {
   const { user, loading } = useAuth();
