ed7c7b6e26cb31f5743f359d5f85b92d0ef9ee81
Manish Hengaju <manish.hju@gmail.com>
2026-05-17 12:44:03 +0545

    socket setup done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,37 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import http from 'http'
+import {Server} from 'socket.io'
+
 import app from "./app.js";
 import {connectDB} from "./db/index.js"
 
 const port = process.env.PORT || 8000;
 
+//http server
+const httpServer = http.createServer(app)
+
+// setup socket server
+const io = new Server(httpServer, {
+  cors: {
+    origin:  "http://localhost:5173",
+    credentials: true,
+  }
+})
+
+// socket connectioni
+io.on("connection", (socket) => {
+  console.log('user connected', socket.id)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id)
+  })
+})
+
 connectDB()
 .then (() => {
-  app.listen(port, () => {
+  httpServer.listen(port, () => {
     console.log("serveer is running on port: ", port)
   })
 })
