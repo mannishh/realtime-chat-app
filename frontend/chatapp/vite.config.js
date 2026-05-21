80dcb15576191c0e49ee9b3432e85e3281ff9be6
Manish Hengaju <manish.hju@gmail.com>
2026-05-21 18:24:13 +0545

    update

--- frontend/chatapp/vite.config.js
+++ frontend/chatapp/vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@shared": path.resolve(__dirname, "../../shared"),
-    },
-  },
-  build: {
-    commonjsOptions: {
-      include: [/shared/, /node_modules/],
-    },
-  },
 });
