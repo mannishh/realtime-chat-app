e5859bac4778596aec07ca556e20bbba07110d15
Manish Hengaju <manish.hju@gmail.com>
2026-05-22 20:58:59 +0545

    add vite-plugin-circular-dependency for circular dependency logging

--- frontend/chatapp/vite.config.js
+++ frontend/chatapp/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import circularDependency from "vite-plugin-circular-dependency";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    circularDependency({
+      outputFilePath: './circular-deps-log.txt' // It will spit out the exact looping files here
+    })
+  ],
 });
